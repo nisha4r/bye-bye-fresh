2140627d6864997485520a287f0943ba0aa81f8c
nisha <nisha.4r@gmail.com>
2023-05-25 22:08:01 -0600

    try catch

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../../models/User');
+const { error } = require('console');
 
 router.post('/login', async (req, res) => {
+    try {
+
+    }catch {error} {
+        res.status(500).json(error);
+    }
+    
+    
+
 });
 
 module.exports = router;
