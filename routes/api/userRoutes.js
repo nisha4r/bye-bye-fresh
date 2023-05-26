bc6f53924006d13b93fe2fbe99a1519b188010b7
nisha <nisha.4r@gmail.com>
2023-05-25 21:46:35 -0600

    post login

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+
+router.post('/login', async (req, res) => {
+});
+
+module.exports = router;
