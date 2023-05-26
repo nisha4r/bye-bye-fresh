b7c7d4f2a8c588f66fc3d3e6bdc42fdc8ad4e65d
nisha <nisha.4r@gmail.com>
2023-05-25 22:13:50 -0600

    login validation

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,17 @@ const { error } = require('console');
 
 router.post('/login', async (req, res) => {
     try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
 
-    }catch {error} {
+        const validPassword = await bcrypt.compare(
+            req.body.password, userData.password
+        );
+        res.status(200).json({ message: 'Login successful' });
+    } catch { error } {
         res.status(500).json(error);
     }
-    
-    
+
+
 
 });
 
