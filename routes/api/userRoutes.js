a5363372523c12b7e8062548bbcc5259ecafa452
nisha <nisha.4r@gmail.com>
2023-05-30 23:40:57 -0600

    login authentication

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,36 +1,61 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const User = require('../../models/User');
+const { User } = require('../../models/User');
 const { error } = require('console');
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
-// post login api to verify username and password
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
-    try {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-        //  querying user table with a given email
-        const userData = await User.findOne({ where: { email: req.body.email } });
-        //  validating user id
-        if (!userData) {
-            res.status(404).json({ message: 'Login failed. Please try again!' });
-            return;
-        }
-        // to compare the hashed password ,used bcrypt library
-        const validPassword = await bcrypt.compare(
-            req.body.password, userData.password
-        );
-        //  validating password , if they do not match, return error message
-        if (!validPassword) {
-            res.status(400).json({ message: 'Login failed. Please try again!' });
-            return;
-        }
-        // for succesful login
-        res.status(200).json({ message: 'Login successful' });
-    } catch { error } {
-        res.status(500).json(error);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
 
+    const validPassword = await userData.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User } = require('../models/User');
 const authentication = require('../utils/authentication');
 router.get('/', async (req, res) => {
     try {
