4428d6a05ff2b54b9640f9540285d5bc99298a01
nisha <nisha.4r@gmail.com>
2023-05-30 20:22:30 -0600

    adding sequelize and dependencies

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
