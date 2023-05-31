456ca172fae07e14671743427ae67be4dca5f699
nisha <nisha.4r@gmail.com>
2023-05-30 20:31:40 -0600

    adding handlebar views

--- server.js
+++ server.js
@@ -29,13 +29,16 @@ const sess = {
 };
 
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
