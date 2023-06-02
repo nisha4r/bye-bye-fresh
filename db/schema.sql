5417a52088fd72949c8ea461d971c04186170372
nisha4r <125222406+nisha4r@users.noreply.github.com>
2023-06-01 19:01:00 -0600

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -27,11 +27,11 @@ CREATE TABLE RecipeIngredient (
 );
 
 -- I believe we need this to handle the user info?
-CREATE TABLE IF NOT EXISTS `user` (
+/* CREATE TABLE IF NOT EXISTS `user` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; */
